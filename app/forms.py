3a12284bf45d669a34a3f380ec6ae8901523e069
Pavel Olifer <p.olifer@glovisrus.com>
2017-04-28 18:41:19 +0300

    preparing to start

--- app/forms.py
+++ app/forms.py
@@ -5,18 +5,18 @@ __author__ = 'polly'
 
 '''
 
-from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField, TextAreaField, SubmitField
+from wtforms import Form
+from wtforms import TextField, BooleanField, TextAreaField, SubmitField, StringField
 from wtforms.validators import DataRequired, Length
 
 
 class LoginForm(Form):
-    openid = TextField('openid', validators=[DataRequired()])
+    openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
 
 
 class EditForm(Form):
-    nickname = TextField('nickname', validators=[DataRequired()])
+    nickname = StringField('nickname', validators=[DataRequired()])
     about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
 
     def __init__(self, original_nickname, *args, **kwargs):
@@ -25,5 +25,5 @@ class EditForm(Form):
 
 
 class FindVacancy(Form):
-    search = TextField('search')
+    search = StringField('search')
     submit = SubmitField('Find')
